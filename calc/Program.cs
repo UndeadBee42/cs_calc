666d1f6841905242ed87f074dce88c122c8ac6aa
Name <you@example.com>
2021-10-06 21:12:12 +0300

    first commit

---
+++ calc/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace calc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool loop = true;
+            while (loop)
+            { 
+                string input = Console.ReadLine();
+                string [] tokens = input.Split(' ');
+                int res = 0;
+                switch (char.Parse(tokens[1])) {
+                    case '+': res = int.Parse(tokens[0]) + int.Parse(tokens[2]); break;
+                    case '-': res = int.Parse(tokens[0]) - int.Parse(tokens[2]); break;
+                    case '*': res = int.Parse(tokens[0]) * int.Parse(tokens[2]); break;
+                    case '/': res = int.Parse(tokens[0]) / int.Parse(tokens[2]); break;
+                }
+                Console.WriteLine(res);
+                
+            }
+            
+        }
+    }
+}
